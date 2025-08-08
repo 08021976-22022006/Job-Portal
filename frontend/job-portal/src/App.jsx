44bb8d9a9b4bd77e6caf4cac87d9876768d858a6
08021976-22022006 <poojanas.dev@gmail.com>
2025-08-08 22:33:29 +0530

    Routing done

--- frontend/job-portal/src/App.jsx
+++ frontend/job-portal/src/App.jsx
@@ -9,6 +9,15 @@ import { Toaster } from "react-hot-toast";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import SignUp from "./pages/Auth/SignUp";
 import Login from "./pages/Auth/Login";
+import JobSeekerDashboard from "./pages/JobSeeker/JobSeekerDashboard";
+import JobDetails from "./pages/JobSeeker/JobDetails";
+import SavedJobs from "./pages/JobSeeker/SavedJobs";
+import UserProfile from "./pages/JobSeeker/UserProfile";
+import EmployerDashboard from "./pages/Employer/EmployerDashboard";
+import JobPostingForm from "./pages/Employer/JobPostingForm";
+import ManageJobs from "./pages/Employer/ManageJobs";
+import ApplicationViewer from "./pages/Employer/ApplicationViewer";
+import EmployerProfilePage from "./pages/Employer/EmployerProfilePage";
 
 const App = () => {
   return (
@@ -19,7 +28,21 @@ const App = () => {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<Navigate to="/" replace />} />
-          
+
+          <Route path="/find-jobs" element={<JobSeekerDashboard />} />
+          <Route path="/job/:jobId" element={<JobDetails />} />
+          <Route path="/saved-jobs" element={<SavedJobs />} />
+          <Route path="/profile" element={<UserProfile />} />
+
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute requiredRole="employer" />}>
+            <Route path="/employer-dashboard" element={<EmployerDashboard />} />
+            <Route path="/post-job" element={<JobPostingForm />} />
+            <Route path="/manage-jobs" element={<ManageJobs />} />
+            <Route path="/applicants" element={<ApplicationViewer />} />
+            <Route path="/company-profile" element={<EmployerProfilePage />} />
+          </Route>
+
         </Routes>
       </Router>
 
