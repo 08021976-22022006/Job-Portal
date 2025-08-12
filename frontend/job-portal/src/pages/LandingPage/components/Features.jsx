a2d344ea2fd82ec5e5b4825b80080fd87f4d106e
08021976-22022006 <poojanas.dev@gmail.com>
2025-08-12 10:19:50 +0530

    Features.jsx

--- frontend/job-portal/src/pages/LandingPage/components/Features.jsx
+++ frontend/job-portal/src/pages/LandingPage/components/Features.jsx
@@ -18,7 +18,7 @@ const Features = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-2 gap-16 lg:gap-24">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-24">
           {/* Job Seeker Dashboard */}
           <div>
             <div className="text-center mb-12">
