cb084e1f588fdd9bb0fbafb58126fdc8d2edbca6
08021976-22022006 <poojanas.dev@gmail.com>
2025-08-12 09:06:03 +0530

    data.js done

---
+++ frontend/job-portal/src/pages/LandingPage/components/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { employerFeatures, jobSeekerFeatures } from "../../../utils/data"
+
+const Features = () => {
+  return (
+    <div>
+      Feature
+    </div>
+  )
+}
+
+export default Features
